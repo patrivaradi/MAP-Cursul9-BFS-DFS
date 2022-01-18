6b151261113ffa1c2d1b17135732d5f4fea68a5d
Patricia Varadi <varadi.patricia@student.uoradea.ro>
2022-01-18 13:46:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cursul9
+{
+    class Program
+    {
+        // To do pt lab:
+        //-bipartit cu parcurgere in adancime
+        //-* sa verificam daca un graf e tripartit
+        public static int[,] matAd;
+        private static bool[] visited;
+        private static int[] ivisited;
+
+        static void Main(string[] args)
+        {
+            matAd = load(@"data.in");
+            view(matAd);
+            Console.WriteLine();
+            Console.WriteLine("BFS:");
+            BFS(0, matAd.GetLength(0));
+            Console.WriteLine();
+            Console.WriteLine();
+           /* Console.WriteLine("Bipartit:");
+            Console.WriteLine(Bipartit(1, matAd.GetLength(0)));
+            Console.WriteLine();
+            Console.WriteLine();
+            ivisited = new int[matAd.GetLength(0)];
+            Console.WriteLine("DFS_Bipartit:");
+            Console.WriteLine(DFS_Bipartit(1, matAd.GetLength(0), 1));
+            Console.WriteLine();
+            Console.WriteLine("DFS:");
+            visited = new bool[matAd.GetLength(0)];
+            DFS(1, matAd.GetLength(0));
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Tripartit:");
+            Console.WriteLine(Tripartit(1, matAd.GetLength(0)));
+           */
+            Console.ReadKey();
+        }
+
+        static int[,] load(string fileName)
+        {
+            int lines, columns;
+            string buffer;
+            List<string> data = new List<string>();
+            System.IO.TextReader dataLoad = new System.IO.StreamReader(fileName);
+            while ((buffer = dataLoad.ReadLine()) != null)
+            {
+                data.Add(buffer);
+            }
+            dataLoad.Close();
+            lines = data.Count();
+            columns = lines;
+            int[,] toReturn = new int[lines, columns];
+            for (int i = 0; i < lines; i++)
+            { 
+                for (int j = 0; j < columns; j++)
+                {
+                    toReturn[i, j] = data[i][j] - 48;
+                }
+            }
+            return toReturn;
+        }
+
+        static void view(int[,] toView)
+        {
+            int lines = toView.GetLength(0);
+            int columns = toView.GetLength(1);
+            for (int i = 0; i < lines; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(toView[i, j] + " ");
+
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void BFS(int start, int n)
+        {
+            Queue q = new Queue();
+            q.Push(start);
+            bool[] visited = new bool[n];
+            visited[start] = true;
+            while(q.values.Length!=0)
+            {
+                int nodCurent = q.Pop();
+                Console.Write(nodCurent + " ");
+                for(int i=0; i<n; i++)
+                {
+                    if(matAd[nodCurent,i]!=0 && visited[i]==false)
+                    {
+                        q.Push(i);
+                        visited[i] = true;
+                    }
+                }
+            }
+        }
+        /*
+        static void DFS(int nodCurent, int n)
+        {
+            Console.Write(nodCurent + " ");
+            visited[nodCurent] = true;
+            for (int i = 0; i < n; i++)
+            {
+                if (matAd[nodCurent, i] != 0 && visited[i] == false)
+                {
+                    DFS(i, n);
+                }
+            }
+        }
+
+        static bool DFS_Bipartit(int nodCurent, int n, int value)
+        {
+            ivisited[nodCurent] = value;
+            for (int i = 0; i < n; i++)
+            {
+                if (matAd[nodCurent, i] != 0)
+                {
+                    if(ivisited[i] == 0)
+					{
+                        if(ivisited[nodCurent] == 1)
+						{
+                            return DFS_Bipartit(i, n, 2);
+                        }
+						else
+						{
+                            return DFS_Bipartit(i, n, 1);
+                        }
+					}
+					else
+					{
+                        if (ivisited[nodCurent] == ivisited[i])
+                            return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        static bool Bipartit(int start, int n)
+        {
+            Queue q = new Queue();
+            q.Push(start);
+            int[] visited = new int[n];
+            visited[start] = 1;
+            while (q.values.Length != 0)
+            {
+                int nodCurent = q.Pop();
+                for (int i = 0; i < n; i++)
+                {
+                    if (matAd[nodCurent, i] != 0 )
+                    {
+                        if(visited[i]==0)
+                        { 
+                            q.Push(i);
+                            if(visited[nodCurent]==1)
+                                 visited[i] = 2;
+                            else
+                                visited[i] = 1;
+                        }
+                        else
+                        {
+                            if (visited[nodCurent] == visited[i])
+                                return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        static bool Tripartit(int start, int n)
+        {
+            Queue q = new Queue();
+            q.Push(start);
+            int[] visited = new int[n];
+            visited[start] = 1;
+            while (q.values.Length != 0)
+            {
+                int nodCurent = q.Pop();
+                for (int i = 0; i < n; i++)
+                {
+                    if (matAd[nodCurent, i] != 0)
+                    {
+                        if (visited[i] == 0)
+                        {
+                            q.Push(i);
+                            if (visited[nodCurent] == 1)
+                                visited[i] = 2;
+                            else
+                                visited[i] = 1;
+                        }
+                        else
+                        {
+                            if (visited[nodCurent] == visited[i])
+                                if (visited[i] == 3)
+                                {
+                                    return false;
+                                }
+								else
+								{
+                                    visited[i] = 3;
+                                }
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+        */
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cursul9
+{
+    public class Stack
+    {
+        ///In stiva noastra adaugam si stergem elemente de la inceputul sirului,
+        ///adica cea mai mare pozitia are elementul pus prima data in stiva
+        int[] values;
+        public Stack()
+        {
+            values = new int[0];
+        }
+
+        public void Push(int ValueToAdd)
+        {
+            int[] temp = new int[values.Length + 1];
+            for (int i = 0; i < values.Length; i++)
+            {
+                temp[i + 1] = values[i];
+            }
+            temp[0] = ValueToAdd;
+            values = temp;
+        }
+
+        public int Pop()
+        {
+            int toReturn = values[0];
+            int[] temp = new int[values.Length - 1];
+            for (int i = 0; i < values.Length - 1; i++)
+                temp[i] = values[i + 1];
+            values = temp;
+            return toReturn;
+        }
+
+        public int Peek()
+        {
+            return values[0];
+        }
+
+        public bool isEmpty()
+        {
+            if (values.Length == 0)
+                return true;
+            return false;
+        }
+
+        public void view()
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                Console.Write(values[i] + " ");
+            }
+        }
+    }
+}
